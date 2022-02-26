672a118acf3fca2acfa5a9e86a15e82abb6197cc
ke vu luu <jack.vuluu.jp@gmail.com>
2022-02-26 20:46:45 +0900

    Move ListNode to common

--- src/vulk/leetcode/common/ListNode.java
+++ src/vulk/leetcode/common/ListNode.java
@@ -1,20 +1,20 @@
-package vulk.leetcode.easy;
+package vulk.leetcode.common;
 
 import vulk.leetcode.util.ComUtil;
 
 public class ListNode {
 
-	int val;
-	ListNode next;
+	public int val;
+	public ListNode next;
 
-	ListNode() {
+	public ListNode() {
 	}
 
-	ListNode(int val) {
+	public ListNode(int val) {
 		this.val = val;
 	}
 
-	ListNode(int val, ListNode next) {
+	public ListNode(int val, ListNode next) {
 		this.val = val;
 		this.next = next;
 	}
